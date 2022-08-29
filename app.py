ddfd95d791b63d6445ea6da394cf27b62c914a0b
Aaditya1612 <83654180+Aaditya1612@users.noreply.github.com>
2022-08-29 12:49:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,9 @@ def result():
             return 'Hi, how can I help you?'
         if request.form["msg"].upper().rstrip('?')== 'WHO ARE YOU?' or request.form["msg"].upper().rstrip('?')=='INTRODUCE YOURSELF' or request.form["msg"].upper().rstrip('?')=='WHAT IS YOUR NAME' or request.form["msg"].upper().rstrip('?')=='WHO ARE YOU' or request.form["msg"].upper().rstrip('?')=='WHAT IS YOUR NAME?':
             return 'I am Lara'
-        if request.form["msg"].upper().rstrip('?')=='WHO MADE YOU' or request.form["msg"].upper().rstrip('?')=='WHO CREATED YOU' or request.form["msg"].upper().rstrip('?')=='WHO IS YOUR DEVELOPER' or request.form["msg"].upper().rstrip('?')=='WHO DEVELOPED YOU' or request.form["msg"].upper().rstrip('?')=='WHO IS YOUR FATHER' or request.form["msg"].upper().rstrip('?')=='WHO IS YOUR MOTHER' or request.form["msg"].upper().rstrip('?')=='WHOSE CREATION YOU ARE':
-            return 'Aaditya has developed me.'
         msg = request.form["msg"]
         res=client.query(msg)
         answer = next(res.results).text
         return answer
     except:
-        return "Sorry, Can't find that. An internal error."    
+        return "Sorry, Can't find that. Please try a different search."    
